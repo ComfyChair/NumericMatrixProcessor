4598867247b59c9c7529c6a2d3f42a7859579d82
ComfyChair <unknown.entity@gmx.de>
2024-10-23 10:19:22 +0200

    Stage 2/6: Multiplication by Number

--- Numeric Matrix Processor (Kotlin)/task/src/processor/Main.kt
+++ Numeric Matrix Processor (Kotlin)/task/src/processor/Main.kt
@@ -3,23 +3,19 @@ package processor
 fun main() {
     val aDims = readDimensions()
     val aMatrix = readMatrix(aDims.first)
-    val bDims = readDimensions()
-    val bMatrix = readMatrix(bDims.first)
-    if (aDims != bDims) {
-        println("ERROR")
-    } else {
-        val result =matrixAddition(aMatrix, bMatrix)
-        printMatrix(result)
-    }
+    val constant = readln().toInt()
+    val result = aMatrix * constant
+    printMatrix(result)
 }
 
-fun printMatrix(result: List<List<Int>>) {
-    println(result.joinToString("\n") { it.joinToString(" ") })
-}
 
-fun matrixAddition(aMatrix: List<List<Int>>, bMatrix: List<List<Int>>) : List<List<Int>> {
-    return List(aMatrix.size) { i -> aMatrix[i].mapIndexed { j, value ->
+operator fun List<List<Int>>.plus(bMatrix: List<List<Int>>) : List<List<Int>> {
+    return List(this.size) { i -> this[i].mapIndexed { j, value ->
         value + bMatrix[i][j]  } }
+}
+
+operator fun List<List<Int>>.times(constant: Int) : List<List<Int>> {
+    return List(this.size) { i -> this[i].map { value -> value * constant } }
 }
 
 fun readDimensions(): Pair<Int, Int> {
@@ -31,3 +27,6 @@ private fun readMatrix(n: Int) = List(n) { readIntRow() }
 
 private fun readIntRow() = readln().split(" ").map(String::toInt)
 
+fun printMatrix(result: List<List<Int>>) {
+    println(result.joinToString("\n") { it.joinToString(" ") })
+}
